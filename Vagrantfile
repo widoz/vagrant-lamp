1eb737c50b2a23bcb4360e524b32f3bbdac048ed
Guido Scialfa <dev@guidoscialfa.com>
2017-12-25 21:18:21 +0100

    First Commit

---
+++ Vagrantfile
@@ -0,0 +1,55 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# A set of Vagrant version requirements can be specified in the Vagrantfile to enforce
+# that people use a specific version of Vagrant with a Vagrantfile.
+# This can help with compatibility issues that may otherwise arise from using a too old or too new
+# Vagrant version with a Vagrantfile.
+# https://www.vagrantup.com/docs/vagrantfile/vagrant_version.html
+Vagrant.require_version ">= 2.0.1"
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+#
+# The most common configuration options are documented and commented below.
+# For a complete reference, please see the online documentation at
+# https://docs.vagrantup.com.
+Vagrant.configure("2") do |config|
+  # Every Vagrant development environment requires a box. You can search for
+  # boxes at https://vagrantcloud.com/search.
+  config.vm.box = "ubuntu/xenial64"
+
+  # The version of the box to use. This defaults to ">= 0" (the latest version available).
+  # This can contain an arbitrary list of constraints, separated by commas, such as: >= 1.0, < 1.5.
+  # When constraints are given, Vagrant will use the latest available box satisfying these constraints.
+  config.vm.box_version = ">= 20171221.0.0"
+
+  # BootStrap
+  config.vm.provision "shell" do |s|
+      s.name = 'WordPress SushiCode'
+      s.path = "inc/bootstrap.sh"
+  end
+
+  # The hostname the machine should have. Defaults to nil. If nil, Vagrant will not manage the hostname.
+  # If set to a string, the hostname will be set on boot.
+  config.vm.hostname = 'wordpress.sushicode'
+
+  # Disable automatic box update checking. If you disable this, then
+  # boxes will only be checked for updates when the user runs
+  # `vagrant box outdated`. This is not recommended.
+  config.vm.box_check_update = false
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine and only allow access
+  # via 127.0.0.1 to disable public access
+  # config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
+
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  config.vm.network "private_network", ip: "192.168.33.10"
+
+  # MySql
+  config.vm.network "forwarded_port", guest: 3306, host: 3306
+end
---
+++ inc/bootstrap.sh
@@ -0,0 +1,324 @@
+#!/usr/bin/env bash
+
+#
+# Write Log in File
+#
+write_log ()
+{
+	echo "$1:$2" >> $LOG_FILE
+	echo "$3" >> $LOG_FILE
+}
+
+#
+# Export Variables
+#
+export DEBIAN_FRONTEND=noninteractive
+
+#
+# Variables
+#
+DBHOST=localhost
+DBNAME=wp
+DBUSER=root
+DBPASSWD=root
+REMOTE_HOST=192.168.33.10
+LOG_FILE=/vagrant/vm_build.log
+PHP_VERSION_LIST=(5.6 7.0 7.1 7.2)
+PHP_DEFAULT_VERSION=7.1
+
+#
+# Apt Get Update
+#
+echo -e "\nUpdating packages list ...\n"
+write_log "APTGET" "Update packages"
+
+apt-get -qq update
+
+#
+# Setup Locales
+#
+echo -e "\nInstall Locales ..."
+write_log "APTGET" "Install Locales ..."
+
+apt-get install -y language-pack-en-base
+apt-get install --reinstall locales
+
+locale-gen --no-purge --lang en_US.UTF-8
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+echo "export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8" >> ~/.bash_profile
+
+dpkg-reconfigure locales
+
+#
+# Add extra repositories
+#
+echo -e "\nAdd Repositories ..."
+write_log "APTGET" "Install Repositories ..."
+
+apt-add-repository -y ppa:ondrej/php
+add-apt-repository -y ppa:git-core/ppa
+
+apt-get update
+
+#
+# Install Bases
+#
+echo -e "\nInstall base packages ..."
+write_log "APTGET" "Install base packages ..."
+
+apt-get -y install vim curl build-essential python-software-properties rubygems ruby-dev 2>> $LOG_FILE
+
+#
+# MySQL setup for development purposes ONLY
+#
+echo -e "\nInstall MySQL specific packages and settings ..."
+write_log "APTGET" "Install MYSQL ..."
+
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
+apt-get -y install mysql-server phpmyadmin 2>> $LOG_FILE
+
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME" 2>> $LOG_FILE
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'" 2>> $LOG_FILE
+
+sed -i "s/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+apt-get -y install php \
+    apache2 \
+    libapache2-mod-php \
+    php-curl \
+    php-gd \
+    php-mysql \
+    php-mongodb \
+    php-xml \
+    php-gettext 2>> $LOG_FILE
+
+a2enmod rewrite 2>> $LOG_FILE
+
+sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+rm -rf /var/www/html
+ln -fs /vagrant /var/www/html
+
+#
+# Install PHP
+#
+echo -e "\nInstall Php ..."
+write_log "PHP" "Configure Stuffs" "Xdebug, phpini, PHPCS ..."
+
+for PHP_VERSION in "${PHP_VERSION_LIST[@]}"; do
+    if [  $(dpkg-query -W -f='${Status}' php${PHP_VERSION} 2>> /dev/null | grep -c "ok installed") -eq 0 ]; then
+        write_log 'INSTALLING PHP ' ${PHP_VERSION}
+    	apt-get -y install php${PHP_VERSION} 2>> $LOG_FILE
+
+        apt-get -y install php${PHP_VERSION}-xml \
+            php${PHP_VERSION}-bz2 \
+            php${PHP_VERSION}-curl \
+            php${PHP_VERSION}-gd \
+            php${PHP_VERSION}-json \
+            php${PHP_VERSION}-mbstring \
+            php${PHP_VERSION}-mysql \
+            php${PHP_VERSION}-mysqli \
+            php${PHP_VERSION}-mysqlnd \
+            php${PHP_VERSION}-zip \
+            php${PHP_VERSION}-opcache \
+            php${PHP_VERSION}-pdo \
+            php${PHP_VERSION}-pdo-mysql \
+            php${PHP_VERSION}-readline \
+            php${PHP_VERSION}-dev \
+            php${PHP_VERSION}-fpm 2>> $LOG_FILE
+    fi
+
+    if [ -f /etc/php/${PHP_VERSION}/apache2/php.ini ]; then
+        sed -i -r "s/;?upload_max_filesize\s*=.*/upload_max_filesize = 100M/g" /etc/php/${PHP_VERSION}/apache2/php.ini
+        sed -i -r "s/;?post_max_size\s*=.*/post_max_size = 100M/g" /etc/php/${PHP_VERSION}/apache2/php.ini
+        sed -i -r "s/;?error_reporting\s*=\s.*/error_reporting = E_ALL/" /etc/php/${PHP_VERSION}/apache2/php.ini
+        sed -i -r "s/;?display_errors\s*=\s.*/display_errors = On/" /etc/php/${PHP_VERSION}/apache2/php.ini
+        sed -i -r "s/;?log_errors\s*=.*/log_errors = On/" /etc/php/${PHP_VERSION}/apache2/php.ini
+        sed -i -r "s/;?error_log\s*=.*/error_log = \/var\/log\/php\/error.log/" /etc/php/${PHP_VERSION}/apache2/php.ini
+
+        if ! grep -Fxq '[XDebug]' /etc/php/${PHP_VERSION}/apache2/php.ini; then
+        echo '
+[XDebug]
+zend_extension="xdebug.so"
+xdebug.remote_enable=1
+xdebug.remote_connect_back=1
+xdebug.profiler_enable=0
+xdebug.profiler_enable_trigger=1
+xdebug.profiler_output_dir="~/xdebug/profiler"
+xdebug.idekey=XDEBUG_ECLIPSE
+xdebug.remote_port=9000
+xdebug.remote_host='${REMOTE_HOST}'
+xdebug.remote_autostart=0
+xdebug.remote_log=/vagrant/xdebug.log
+xdebug.max_nesting_level=100
+        ' >> /etc/php/${PHP_VERSION}/apache2/php.ini
+        fi
+    fi
+
+    if [ ! -f /usr/bin/switch_php.sh ]; then
+        echo "
+#!/bin/bash
+
+# Set current php version
+PHP_CURRENT=\$1
+
+# If no version has been retrieved
+if [ ! $PHP_CURRENT ] ;then
+	exit 1
+fi
+
+# Set the php version to activate
+PHP_TO_ACTIVATE=\$2
+
+# If no version is passed to the script
+if [ ! $PHP_TO_ACTIVATE ] ;then
+	exit 1
+fi
+
+echo -e \"Disable php \"$PHP_CURRENT
+# Dismod current php version
+sudo a2dismod php$PHP_CURRENT 1> /dev/null 2>&1
+
+echo -e \"Enabling php \"$PHP_TO_ACTIVATE
+# Then enmod the one passed to the script
+sudo a2enmod php$PHP_TO_ACTIVATE 1> /dev/null 2>&1
+
+# Don't forget php-config and php cli
+if [ -f /usr/bin/php$PHP_TO_ACTIVATE ]; then
+	sudo update-alternatives --set php /usr/bin/php$PHP_TO_ACTIVATE 1> /dev/null 2>&1
+fi
+
+if [ -f /usr/bin/php-config$PHP_TO_ACTIVATE ]; then
+	sudo update-alternatives --set php-config /usr/bin/php-config$PHP_TO_ACTIVATE 1> /dev/null 2>&1
+fi
+
+# Last thing, restart apache
+sudo service apache2 restart 1> /dev/null 2>&1
+        " >> /usr/bin/switch_php.sh
+
+        chmod 755 /usr/bin/switch_php.sh
+        ln -s /usr/bin/switch_php.s /usr/bin/switch_php
+    fi
+done
+
+apt-get install php-xdebug 2>> $LOG_FILE
+
+a2dismod php7.2
+a2enmod php$PHP_DEFAULT_VERSION
+update-alternatives --set php /usr/bin/php$PHP_DEFAULT_VERSION 2>> $LOG_FILE
+
+pear channel-update pear.php.net 2>> $LOG_FILE
+pear install PHP_CodeSniffer
+
+#
+# Node
+#
+echo -e "\nInstall Node"
+write_log "APTGET" "Install Node ..."
+
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+
+if [ -f nodesource_setup.sh ]; then
+    sh nodesource_setup.sh 2>> $LOG_FILE
+    apt-get -y install nodejs 2>> $LOG_FILE
+fi
+
+#
+# Git
+#
+echo -e "\nInstall Git ..."
+write_log "APTGET" "Install Git ..."
+
+if [  $(dpkg-query -W -f='${Status}' git 2>> /dev/null | grep -c "ok installed") -eq 0 ]; then
+	apt-get -y install git 2>> $LOG_FILE
+else
+	apt-get -y install --only-upgrade git 2>> $LOG_FILE
+fi
+
+#
+# Composer
+#
+echo -e "\nInstall Composer ..."
+write_log "COMPOSER" "Install ..."
+
+if [ ! -f /usr/bin/composer ]; then
+
+    if [  $(dpkg-query -W -f='${Status}' composer 2>> /dev/null | grep -c "ok installed") -eq 0 ]; then
+    	EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+    	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" 2>> $LOG_FILE
+    	ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+    	if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
+            write_log 'Error' 'Invalid installer signature'
+        	rm composer-setup.php
+     	  	exit 1
+    	fi
+    fi
+
+    php composer-setup.php --quiet 2>> $LOG_FILE
+    rm composer-setup.php
+
+    if [ ! composer.phar ]; then
+        write_log "Composer Phar" "File Not found"
+    else
+        mv composer.phar /usr/bin/composer.phar
+        ln -s /usr/bin/composer.phar /usr/bin/composer
+        chmod 755 /usr/bin/composer.phar
+    fi
+
+fi
+
+#
+# Task Runners & Package Managers
+#
+echo -e "\nInstall Task Runners and Package Managers ..."
+write_log "NPM" "Install Task Runners and Package Managers ..."
+
+npm install -g bower 2>> $LOG_FILE
+npm install -g grunt 2>> $LOG_FILE
+npm install -g gulp 2>> $LOG_FILE
+
+#
+# Pre Processors
+#
+echo -e "\nInstall Css Preprocessors ..."
+write_log "Css" "Install Preprocessors ..."
+
+sudo gem install sass --no-user-install 2>> $LOG_FILE
+
+#
+# WordPress Stuffs
+#
+echo -e "\nInstall WP Stuffs ..."
+write_log 'WP' 'Install CLI ...'
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 1> /dev/null 2>> $LOG_FILE
+
+if [ -f wp-cli.phar ]; then
+    php wp-cli.phar --info 2>> $LOG_FILE
+    chmod +x wp-cli.phar 2>> $LOG_FILE
+    sudo mv wp-cli.phar /usr/bin/wp 2>> $LOG_FILE
+else
+    write_log 'WPCLI' 'No way to locate the file wp-cli.phar'
+fi
+
+#
+# Restart Services
+#
+echo -e "\nRestart Services ..."
+write_log 'SERV' 'Restart Services ...'
+
+service apache2 restart 2>> $LOG_FILE
