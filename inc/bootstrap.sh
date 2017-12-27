6988ee4fcabe02fb98f5860ad67c56a985e99471
Guido Scialfa <dev@guidoscialfa.com>
2017-12-27 10:58:32 +0100

    Fix variables are expanded but string must be used.

--- inc/bootstrap.sh
+++ inc/bootstrap.sh
@@ -177,7 +177,7 @@ if [ ! -f /usr/bin/switch_php.sh ]; then
 PHP_CURRENT=\$1
 
 # If no version has been retrieved
-if [ ! $PHP_CURRENT ] ;then
+if [ ! \$PHP_CURRENT ] ;then
 	exit 1
 fi
 
@@ -185,25 +185,25 @@ fi
 PHP_TO_ACTIVATE=\$2
 
 # If no version is passed to the script
-if [ ! $PHP_TO_ACTIVATE ] ;then
+if [ ! \$PHP_TO_ACTIVATE ] ;then
 	exit 1
 fi
 
-echo -e \"Disable php \"$PHP_CURRENT
+echo -e \"Disable php \"\$PHP_CURRENT
 # Dismod current php version
-sudo a2dismod php$PHP_CURRENT 1> /dev/null 2>&1
+sudo a2dismod php\$PHP_CURRENT 1> /dev/null 2>&1
 
-echo -e \"Enabling php \"$PHP_TO_ACTIVATE
+echo -e \"Enabling php \"\$PHP_TO_ACTIVATE
 # Then enmod the one passed to the script
-sudo a2enmod php$PHP_TO_ACTIVATE 1> /dev/null 2>&1
+sudo a2enmod php\$PHP_TO_ACTIVATE 1> /dev/null 2>&1
 
 # Don't forget php-config and php cli
-if [ -f /usr/bin/php$PHP_TO_ACTIVATE ]; then
-	sudo update-alternatives --set php /usr/bin/php$PHP_TO_ACTIVATE 1> /dev/null 2>&1
+if [ -f /usr/bin/php\$PHP_TO_ACTIVATE ]; then
+	sudo update-alternatives --set php /usr/bin/php\$PHP_TO_ACTIVATE 1> /dev/null 2>&1
 fi
 
-if [ -f /usr/bin/php-config$PHP_TO_ACTIVATE ]; then
-	sudo update-alternatives --set php-config /usr/bin/php-config$PHP_TO_ACTIVATE 1> /dev/null 2>&1
+if [ -f /usr/bin/php-config\$PHP_TO_ACTIVATE ]; then
+	sudo update-alternatives --set php-config /usr/bin/php-config\$PHP_TO_ACTIVATE 1> /dev/null 2>&1
 fi
 
 # Last thing, restart apache
